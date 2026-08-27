ac9b010eeb09b7d45bc65469e118379c5f66b22b
prgmr99 <johnyeom24@naver.com>
2026-08-27 16:13:00 +0900

    feat: GitHub Pages 배포 설정 추가 및 코드 정리

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  site: "https://prgmr99.github.io",
+  base: "/streaming-ssr-from-scratch",
+  integrations: [react(), mdx()],
 });
